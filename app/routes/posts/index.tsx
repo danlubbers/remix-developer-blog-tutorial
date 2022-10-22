4fc126e6a2fe8f9d8cfa64450467196ac4787c98
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-22 12:53:13 -0400

    Completed the remix developer blog tutorial from the remix docs website.

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,35 @@
+import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/post.server";
+
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const posts = await getPosts();
+  return json<LoaderData>({ posts });
+};
+
+export default function PostsRoute() {
+  const { posts } = useLoaderData() as LoaderData;
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
